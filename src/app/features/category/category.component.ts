664832d063d45e57960ba4d6423d36b3bcae05b2
Fabricio <fabriciorochapalmeira@gmail.com>
2023-12-29 20:03:47 -0300

    feat: create abstract class for basic features

--- src/app/features/category/category.component.ts
+++ src/app/features/category/category.component.ts
@@ -3,114 +3,39 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MessageService } from 'primeng/api';
 import { Paginator, PaginatorState } from 'primeng/paginator';
+import { BasicModelResource } from 'src/app/core/models/basic-resource.model';
 import { Category } from 'src/app/core/models/category.model';
 import { Pageable } from 'src/app/core/models/pageable.model';
 import { CategoryService } from 'src/app/core/services/category.service';
+import { AbstractBasicFeatureComponent } from 'src/app/shared/abstract-basic-feature.component';
 
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
 })
-export class CategoryComponent implements OnInit {
-  public categories: Pageable<Category>;
-
-  public visibleModal: boolean = false;
-
-  public form: FormGroup;
-
-  first: number = 0;
-
-  rows: number = 5;
-
-  @ViewChild('paginator', { static: true }) paginator: Paginator;
-
+export class CategoryComponent
+  extends AbstractBasicFeatureComponent<
+    Category,
+    BasicModelResource<Category>,
+    CategoryService
+  >
+  implements OnInit
+{
   constructor(
     private fb: FormBuilder,
     private categoryService: CategoryService,
-    private messageService: MessageService
-  ) {}
+    messageService: MessageService
+  ) {
+    super(messageService, categoryService);
+  }
 
   ngOnInit(): void {
-    this.categoryService.get({ page: 0, size: 5 }).subscribe((data) => {
-      this.categories = data;
+    this.categoryService.get({ page: 0, size: 10 }).subscribe((data) => {
+      this.data = data;
     });
     this.form = this.fb.group({
       id: [''],
       descricao: [''],
     });
-  }
-
-  public openModal(): void {
-    this.form.reset();
-    this.visibleModal = true;
-  }
-
-  public openModalToUpdate(category: Category): void {
-    this.visibleModal = true;
-    this.form.patchValue(category);
-  }
-
-  public saveCategory(): void {
-    if (this.form.get('id')?.value) {
-      this.categoryService.update(this.form.value).subscribe(
-        (data) => {
-          this.visibleModal = false;
-          this.showSucess('Cadastro atualizado com sucesso');
-          this.form = this.fb.group({
-            id: [''],
-            descricao: [''],
-          });
-          this.categoryService.get({ page: 0, size: 5 }).subscribe((data) => {
-            this.paginator.changePage(data.totalPages - 1);
-          });
-        },
-        (err: HttpErrorResponse) => {
-          this.showErrors(err);
-        }
-      );
-      return;
-    }
-
-    this.categoryService.create(this.form.value).subscribe(
-      (data) => {
-        this.visibleModal = false;
-        this.showSucess();
-        this.categoryService.get({ page: 0, size: 5 }).subscribe((data) => {
-          this.categories = data;
-          this.paginator.changePage(data.totalPages - 1);
-        });
-        this.form = this.fb.group({
-          id: [''],
-          descricao: [''],
-        });
-      },
-      (err: HttpErrorResponse) => {
-        this.showErrors(err);
-      }
-    );
-  }
-
-  public showSucess(message: string = 'Cadastro realizado com sucesso') {
-    this.messageService.add({
-      severity: 'success',
-      summary: 'Successo',
-      detail: message,
-    });
-  }
-
-  public showErrors(err: HttpErrorResponse) {
-    this.messageService.add({
-      severity: 'error',
-      summary: 'Erro',
-      detail: err?.error?.erros?.map((e: any) => e.detalhe).join('\n'),
-    });
-  }
-
-  public onPageChange(event: PaginatorState): void {
-    this.categoryService
-      .get({ page: event.page, size: event.rows })
-      .subscribe((data) => {
-        this.categories = data;
-      });
   }
 }
---
+++ src/app/shared/abstract-basic-feature.component.ts
@@ -0,0 +1,105 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, ViewChild } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { MessageService } from 'primeng/api';
+import { Paginator, PaginatorState } from 'primeng/paginator';
+import { BasicResourceService } from '../core/services/basic-resource.service';
+import { BasicModelResource } from '../core/models/basic-resource.model';
+import { Pageable } from '../core/models/pageable.model';
+import { Inject } from '@angular/core';
+
+@Component({
+  template: '',
+})
+export class AbstractBasicFeatureComponent<
+  M,
+  T extends BasicModelResource<M>,
+  S extends BasicResourceService<T>
+> {
+  @ViewChild('paginator', { static: true }) paginator: Paginator;
+
+  public visibleModal: boolean = false;
+
+  public form: FormGroup;
+
+  public data: Pageable<T>;
+
+  public first: number = 0;
+
+  public rows: number = 10;
+
+  public constructor(
+    private messageService: MessageService,
+    @Inject('BasicResourceService') private service: S
+  ) {}
+
+  public onSubmit = (): void => {
+    if (this.form.get('id')?.value) {
+      this.service.update(this.buildRequest()).subscribe({
+        next: () => {
+          this.onSuccessSave('Cadastro atualizado com sucesso');
+        },
+        error: (err: HttpErrorResponse) => {
+          this.showErrors(err);
+        },
+      });
+      return;
+    } else {
+      this.service.create(this.buildRequest()).subscribe({
+        next: () => {
+          this.onSuccessSave();
+        },
+        error: (err: HttpErrorResponse) => {
+          this.showErrors(err);
+        },
+      });
+    }
+  };
+
+  public openModal(): void {
+    this.form.reset();
+    this.visibleModal = true;
+  }
+
+  public openModalToUpdate(data: T): void {
+    this.visibleModal = true;
+    this.form.patchValue(data);
+  }
+
+  public showSucess(message: string) {
+    this.messageService.add({
+      severity: 'success',
+      summary: 'Successo',
+      detail: message,
+    });
+  }
+
+  public showErrors(err: HttpErrorResponse) {
+    this.messageService.add({
+      severity: 'error',
+      summary: 'Erro',
+      detail: err?.error?.erros?.map((e: any) => e.detalhe).join('\n'),
+    });
+  }
+
+  public onSuccessSave(msg = 'Cadastro realizado com sucesso') {
+    this.form.reset();
+    this.showSucess(msg);
+    this.visibleModal = false;
+    this.service.get({ page: 0, size: 10 }).subscribe((data) => {
+      this.paginator.changePage(data.totalPages - 1);
+    });
+  }
+
+  public onPageChange(event: PaginatorState): void {
+    this.service
+      .get({ page: event.page, size: event.rows })
+      .subscribe((data) => {
+        this.data = data;
+      });
+  }
+
+  public buildRequest(): void {
+    return this.form.value;
+  }
+}
